a041ca46e75e950b7e4d90edaa784f4170503485
Cha-12 <81138379+Cha-12@users.noreply.github.com>
2021-07-24 11:49:47 +0530

    Update Que1.java

--- Que1.java
+++ Que1.java
@@ -6,10 +6,13 @@ public class Que1 {
 	
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the value of x");
 		int x = sc.nextInt();
-		int y = sc.nextInt();		
+		System.out.println("Enter the value of y");
+		int y = sc.nextInt();
+		int i;
 		int result = 1;
-		for(y=1;y<=5;y++) {
+		for(i=1;i<=y;i++) {
 			result = result*x;}
 			System.out.println(result);
 		
